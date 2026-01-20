fd125e8fd57af2640da51a3cdf3ca2794ae5d1d0
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-01-20 11:44:01 +0530

    Display Menu Commitment

--- inventory-system/src/main/java/main/Main.java
+++ inventory-system/src/main/java/main/Main.java
@@ -4,6 +4,12 @@ import java.sql.Connection;
 
 import util.DBConnection;
 
+import dao.ProductDAO;
+import model.Product;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Connection con = DBConnection.getConnection();
@@ -12,5 +18,44 @@ public class Main {
         } else {
             System.out.println("Database Connection Failed");
         }
+
+        Scanner sc = new Scanner(System.in);
+        ProductDAO productDAO = new ProductDAO();
+
+        int choice;
+
+        do {
+            // Display menu
+            System.out.println("\n===== MINI ERP - INVENTORY SYSTEM =====");
+            System.out.println("1. Add Product");
+            System.out.println("2. View All Products");
+            System.out.println("3. Update Product");
+            System.out.println("4. Delete Product");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice){
+                case 1:
+                    addProduct(sc, productDAO);
+                    break;
+                case 2:
+                    viewAllProducts(productDAO);
+                    break;
+                case 3:
+                    updateProduct(sc, productDAO);
+                    break;
+                case 4:
+                    deleteProduct(sc, productDAO);
+                    break;
+                case 0:
+                    System.out.println("Exiting system..");
+                    break;
+                default:
+                    System.out.println("Inavlid choice!");
+            }
+
+        } while (choice !=0);
     }
 }
